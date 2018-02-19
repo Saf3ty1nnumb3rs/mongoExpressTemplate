4b97043bf5a9cdf34d3b96ba1f6cbcc2bf8d0c65
Josh Sample <joshuawsample@gmail.com>
2018-02-19 17:43:10 -0500

    Added show route with test

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -6,17 +6,28 @@ const Other = require('../models/other')//from other.js
 
 
 
-//INDEX ROUTE
+//INDEX ROUTE------------------------------------------------------------------------
 
 router.get('/', (req, res) =>{
-    res.send('Hello')///for testing purposes
+    res.send('Hello')///for testing purposes - remove when seeded
 //Use mongo to find all 'Generic' in our database - from ../models/generic.js aka User
     Generic.find()
         .then((generics) => {
             //Once 'generics' have been found, then send JSON object to Client
             res.send(generics)
         })
-
+})
+//SHOW ROUTE------------------------------------------------------------------------
+router.get('/:id', (req, res) => {
+    res.send('Sup')///for testing purposes -remove when seeded
+    //Find the _id of the user we want to return (in our router params)
+    const genericId = req.params.id
+    //Use the _id to search for a specific User in our DB
+    Generic.findById(genericId)
+    .then((generic)=> {
+        res.send(generic)
+    })
+    //Once that user is found, send the object with user data to our
 
 })
 
